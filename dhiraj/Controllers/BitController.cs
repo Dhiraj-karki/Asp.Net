913ae60ff66bb9fa03aa89ed7d359933e9b36aca
dhiraj-karki <karkidhiraj56@gmail.com>
2022-03-14 09:11:11 +0545

    Initial Commit

--- dhiraj/Controllers/BitController.cs
+++ dhiraj/Controllers/BitController.cs
@@ -2,6 +2,7 @@
 using dhiraj.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,7 +13,7 @@ namespace dhiraj.Controllers
     {
         StudentEntities db = new StudentEntities();
         // GET: Bit
-        public ActionResult Index()                                                                                                                           
+        public ActionResult Index()
         {
             List<Std> all_data = db.Stds.ToList();
             return View(all_data);
@@ -26,14 +27,25 @@ namespace dhiraj.Controllers
         {
             return View();
         }
+
+        public ActionResult Edit(int id)
+        {
+            Std std = db.Stds.Find(id);//find data using primary key
+            return View(std);
+        }
         public ActionResult savedata(Std employee)
         {
             db.Stds.Add(employee);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult updatedata(Std employee)
+        {
+            db.Entry(employee).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
     }
 }
-
     
    
